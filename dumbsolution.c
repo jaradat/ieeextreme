6105fb6543bbb28ae22c7fe1447b67a36135e73b
pearl-ambitions <subodha.c.charles@ieee.org>
2016-10-07 19:26:16 -0400

    Create dumbsolution.c

---
+++ dumbsolution.c
@@ -0,0 +1,9 @@
+for (int r = 0 r < H - R + 1; ++r)
+  for (int c = 0; c < W - C + 1; ++c) {
+    //for every (r,c)…
+    int sum = 0;
+    for (int y = 0; y < R; ++y)
+      for (int x = 0; x < C; ++x)
+        sum += G[r + y][c + x];
+    best = min(best, sum);
+  }
