e9b74caf736f774927c04b8d8fded9bbcbb28200
pearl-ambitions <subodha.c.charles@ieee.org>
2016-10-07 20:00:36 -0400

    Create template2.cpp

---
+++ template2.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#inclue<vector>
+#include<map>
+#include<algorithms>
+#include<cmath>
+
+using namespace std;
+
+#define	ll long long
+#define	fr (i, n) for (int (i)=0; (i)<(n); ++(i))
+
+void read()
+{
+
+
+}
+
+void process()
+{
+
+
+}
+
+void output()
+{
+
+
+}
+
+int	main() {
+    read();
+    process();
+    output();
+    return	0;
+}
